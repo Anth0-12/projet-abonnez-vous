47f60843e340c0790580d1d83234b980d4c5e856
Anth0-12 <antho62680@live.fr>
2024-12-15 21:13:41 +0100

    Ajout de la fonction changement de class d'icone au clic

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Je séléctionne et je stocke
 // Icone contient le premier élément <i> du code HTML
 const icone = document.querySelector("i");
-console.log(icone);
+
+// Je soumets l'icone à une action au clic
+icone.addEventListener("click", function () {
+  // J'ajoute la classe "fa-face-smile-wink" et je désactive la classe "fa-face-meh-blank"
+  icone.classList.add("fa-face-smile-wink");
+  icone.classList.toggle("fa-face-meh-blank");
+  icone.classList.toggle("happy");
+});
