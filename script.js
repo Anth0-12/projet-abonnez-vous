c73b2bd808e3debed95b11e085e12342a3fe8380
Anth0-12 <antho62680@live.fr>
2024-12-17 01:59:06 +0100

    Actualisation click icone + création fonction changement bouton abonné

--- script.js
+++ script.js
@@ -2,10 +2,25 @@
 // Icone contient le premier élément <i> du code HTML
 const icone = document.querySelector("i");
 
+//btnButton contient le premier élément avec la classe "btn"
+const btnButton = document.querySelector(".btn");
+console.log(btnButton);
+
 // Je soumets l'icone à une action au clic
 icone.addEventListener("click", function () {
-  // J'ajoute la classe "fa-face-smile-wink" et je désactive la classe "fa-face-meh-blank"
-  icone.classList.add("fa-face-smile-wink");
+  // Je désactive la classe "fa-face-meh-blank", j'ajoute et active la classe "fa-face-smile-wink"
   icone.classList.toggle("fa-face-meh-blank");
+  icone.classList.toggle("fa-face-smile-wink");
   icone.classList.toggle("happy");
 });
+
+// Je soumets le bouton à une action au clic
+btnButton.addEventListener("click", function () { // Au clic
+  if (btnButton.innerText === "Abonné") { // Si le bouton est "Abonné"
+      btnButton.innerText = "Abonnez-vous"; // Alors le bouton devient "Abonnez-vous"
+  } else { // Sinon (si le bouton est "Abonnez-vous")
+      btnButton.innerText = "Abonné"; // Le bouton devient "Abonné"
+  } 
+btnButton.classList.toggle("abonne"); // Ajoute la classe "abonne" au bouton pour modifier le style
+});
+
